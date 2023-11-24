0f9d94d0e0a95d8e26228c8affb93d4fadf8caeb
“@Kevine12” <ikevine2016@gmail.com>
2023-11-24 09:30:40 +0200

    adding question data

--- courser.py
+++ courser.py
@@ -47,3 +47,30 @@ print(digits(25))   # Should print 2
 print(digits(144))  # Should print 3
 print(digits(1000)) # Should print 4
 print(digits(0))    # Should print 1
+
+
+# Question 4
+# Fill in the blanks to complete the “sequence” function. This function should print a sequence of numbers in descending order, from the given "high" variable to the given "low" variable.  The range should make the loop run two times. Complete the range sequences in the nested loops so that the “sequence(1, 3)” function call prints the following:
+
+# 3, 2, 1
+
+# 3, 2, 1:"
+
+# def sequence(low, high):
+#     # Complete the outer loop range to make the loop run twice
+#     # to create two rows
+#     for x in range(___): 
+#         # Complete the inner loop range to print the given variable
+#         # numbers starting from "high" to "low" 
+#         # Hint: To decrement a range parameter, use negative numbers
+#         for y in range(___): 
+#             if y == low:
+#                 # Don’t print a comma after the last item
+#                 print(str(y)) 
+#             else:
+#                 # Print a comma and a space between numbers
+#                 print(str(y), end=", ") 
+
+# sequence(1, 3)
+# # Should print the sequence 3, 2, 1 two times, as shown above.
+# "
