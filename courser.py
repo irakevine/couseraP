167013ac9bf817fd62d97586b2633f21e85df643
“@Kevine12” <ikevine2016@gmail.com>
2023-11-24 09:26:38 +0200

    adding data

--- courser.py
+++ courser.py
@@ -7,3 +7,7 @@
 # # Should print 1 2 3 4 5 6 7 
 
 
+number = 1  # Initialize the variable
+while number <= 7:  # Complete the while loop condition
+    print(number, end=" ")
+    number += 1  # Increment the variable
