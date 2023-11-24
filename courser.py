832da19e9514bdebec35b26185cb5f13a2b6f73f
“@Kevine12” <ikevine2016@gmail.com>
2023-11-24 09:24:22 +0200

    set up folder structure

---
+++ courser.py
@@ -0,0 +1,9 @@
+# Fill in the blanks to print the numbers 1 through 7.:"
+# number = ___ # Initialize the variable
+# while ___ # Complete the while loop condition
+#     print(number, end=" ")
+#     number ___ # Increment the variable
+
+# # Should print 1 2 3 4 5 6 7 
+
+
