29d8dbb6484ec0aff4997fa0e2c4e35de0320b55
“@Kevine12” <ikevine2016@gmail.com>
2023-12-01 15:05:44 +0200

    adding data

--- courser.py
+++ courser.py
@@ -160,3 +160,6 @@ print(get_word("This is a lesson about lists", -4))  # Nothing
 
 print(get_word("Now we are cooking!", 1))  # Should print: Now
 print(get_word("Now we are cooking!", 5))  # Nothing
+
+
+# Let's use tuples to store information about a file: its name, its type and its size in bytes. Fill in the gaps in this code to return the size in kilobytes (a kilobyte is 1024 bytes) up to 2 decimal places. 
