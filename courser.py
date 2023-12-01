9118012327f19d8de6801711ff937132bc292456
“@Kevine12” <ikevine2016@gmail.com>
2023-12-01 16:15:42 +0200

    adding data

--- courser.py
+++ courser.py
@@ -182,3 +182,7 @@ def skip_elements(elements):
 
 print(skip_elements(["a", "b", "c", "d", "e", "f", "g"]))  # Should be ['a', 'c', 'e', 'g']
 print(skip_elements(['Orange', 'Pineapple', 'Strawberry', 'Kiwi', 'Peach']))  # Should be ['Orange', 'Strawberry', 'Peach']
+
+
+
+# The odd_numbers function returns a list of odd numbers between 1 and n, inclusively. Fill in the blanks in the function, using list comprehension. Hint: remember that list and range counters start at 0 and end at the limit minus 1.
