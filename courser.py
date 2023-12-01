af73efeb094d4da39cfc1239f7de9cc5775fed8d
“@Kevine12” <ikevine2016@gmail.com>
2023-12-01 15:01:07 +0200

    adding data

--- courser.py
+++ courser.py
@@ -147,3 +147,16 @@ print(divisible(144, 17))  # Should be 9
 # print(get_word("Now we are cooking!", 5)) # Nothing
 
 
+
+def get_word(sentence, n):
+    if n > 0:
+        words = sentence.split()
+        if n <= len(words):
+            return words[n - 1]
+    return ""
+
+print(get_word("This is a lesson about lists", 4))  # Should print: lesson
+print(get_word("This is a lesson about lists", -4))  # Nothing
+
+print(get_word("Now we are cooking!", 1))  # Should print: Now
+print(get_word("Now we are cooking!", 5))  # Nothing
