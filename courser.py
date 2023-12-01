dec19a450133b08621deecca2d4aa60124200e32
“@Kevine12” <ikevine2016@gmail.com>
2023-12-01 15:14:15 +0200

    adding data

--- courser.py
+++ courser.py
@@ -172,3 +172,13 @@ def file_size(file_info):
 print(file_size(('Class Assignment', 'docx', 17875))) 
 print(file_size(('Notes', 'txt', 496)))  
 print(file_size(('Program', 'py', 1239)))  
+
+
+# Try out the enumerate function for yourself in this quick exercise. Complete the skip_elements function to return every other element from the list, this time using the enumerate function to check if an element is in an even position or an odd position.
+
+
+def skip_elements(elements):
+    return [element for index, element in enumerate(elements) if index % 2 == 0]
+
+print(skip_elements(["a", "b", "c", "d", "e", "f", "g"]))  # Should be ['a', 'c', 'e', 'g']
+print(skip_elements(['Orange', 'Pineapple', 'Strawberry', 'Kiwi', 'Peach']))  # Should be ['Orange', 'Strawberry', 'Peach']
