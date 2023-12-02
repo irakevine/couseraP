c22d9655f27a67b4576d6b8662e6e561c68ceb6c
“@Kevine12” <ikevine2016@gmail.com>
2023-12-02 20:34:30 +0200

    adding data

--- courser.py
+++ courser.py
@@ -223,3 +223,10 @@ print(pig_latin("hello how are you"))  # Should be "ellohay owhay reaay ouyay"
 print(pig_latin("programming in python is fun"))  # Should be "rogrammingpay niay ythonpay siay unfay"
 
 # The guest_list function reads in a list of tuples with the name, age, and profession of each party guest, and prints the sentence "Guest is X years old and works as __." for each one. For example, guest_list(('Ken', 30, "Chef"), ("Pat", 35, 'Lawyer'), ('Amanda', 25, "Engineer")) should print out: Ken is 30 years old and works as Chef. Pat is 35 years old and works as Lawyer. Amanda is 25 years old and works as Engineer. Fill in the gaps in this function to do that.
+
+def guest_list(guests):
+    for guest in guests:
+        name, age, profession = guest
+        print("{} is {} years old and works as {}.".format(name, age, profession))
+
+guest_list([('Ken', 30, "Chef"), ("Pat", 35, 'Lawyer'), ('Amanda', 25, "Engineer")])
