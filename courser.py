0e2644741403191819ab959c9ad958e8132d08ac
“@Kevine12” <ikevine2016@gmail.com>
2023-12-07 15:27:45 +0200

    Documenting Functions, Classes, and Methods (Optional)

--- courser.py
+++ courser.py
@@ -367,3 +367,13 @@ print(some_person.greeting())
 
 
 # Remember our Person class from the last video? Let’s add a docstring to the greeting method. How about, “Outputs a message with the name of the person”.
+
+class Person:
+    def __init__(self, name):
+        self.name = name
+    
+    def greeting(self):
+        """Outputs a message with the name of the person."""
+        print("Hello! My name is {name}.".format(name=self.name))
+
+help(Person.greeting)
