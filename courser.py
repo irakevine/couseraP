7bf852371e25efb91ff576dc274a1cd748a8a7d6
“@Kevine12” <ikevine2016@gmail.com>
2023-12-07 14:47:52 +0200

    adding data

--- courser.py
+++ courser.py
@@ -249,3 +249,5 @@ this_pun_is_for_you = "A little Python humor!"
 print("Roses are {},".format(rose.color))
 print("violets are {},".format(violet.color))
 print(this_pun_is_for_you)
+
+# Creating new instances of class objects can be a great way to keep track of values using attributes associated with the object. The values of these attributes can be easily changed at the object level.  The following code illustrates a famous quote by George Bernard Shaw, using objects to represent people. Fill in the blanks to make the code satisfy the behavior described in the quote. 
