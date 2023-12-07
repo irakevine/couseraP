fe4e978606c548252ca9f30e63ab197842e4ba72
“@Kevine12” <ikevine2016@gmail.com>
2023-12-07 15:40:12 +0200

    adding data

--- courser.py
+++ courser.py
@@ -382,3 +382,41 @@ help(Person.greeting)
 # What was your experience with the lab? Did anything surprise you? Did you have any trouble with the lab?
 
 # Let’s expand a bit on our Clothing classes from the previous in-video question. Your mission: Finish the "Stock_by_Material" method and iterate over the amount of each item of a given material that is in stock. When you’re finished, the script should add up to 10 cotton clothing items.
+
+class Clothing:
+    stock = {'name': [], 'material': [], 'amount': []}
+
+    def __init__(self, name):
+        self.name = name
+        self.material = ""
+
+    def add_item(self, name, material, amount):
+        Clothing.stock['name'].append(name)
+        Clothing.stock['material'].append(material)
+        Clothing.stock['amount'].append(amount)
+
+    def Stock_by_Material(self, material):
+        count = 0
+        n = 0
+        for item in Clothing.stock['material']:
+            if item == material:
+                count += Clothing.stock['amount'][n]
+            n += 1
+        return count
+
+class shirt(Clothing):
+    def __init__(self, name):
+        super().__init__(name)
+        self.material = "Cotton"
+
+class pants(Clothing):
+    def __init__(self, name):
+        super().__init__(name)
+        self.material = "Cotton"
+
+polo = shirt("Polo")
+sweatpants = pants("Sweatpants")
+polo.add_item(polo.name, polo.material, 4)
+sweatpants.add_item(sweatpants.name, sweatpants.material, 6)
+current_stock = polo.Stock_by_Material("Cotton")
+print(current_stock)
