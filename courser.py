15d1467e640b9b2a6d1c7c9be96fc9d45d0f45b4
“@Kevine12” <ikevine2016@gmail.com>
2023-12-07 15:16:44 +0200

    adding data

--- courser.py
+++ courser.py
@@ -296,3 +296,33 @@ def describe_furniture(piece):
 
 print(describe_furniture(table))  # Output: "This piece of furniture is made of brown wood"
 print(describe_furniture(couch))  # Output: "This piece of furniture is made of red leather"
+
+
+class Person:
+    apples = 0
+    ideas = 0
+
+johanna = Person()
+johanna.apples = 1
+johanna.ideas = 1
+
+martin = Person()
+martin.apples = 2
+martin.ideas = 1
+
+def exchange_apples(you, me):
+    # Exchange all apples between 'you' and 'me'
+    you.apples, me.apples = me.apples, you.apples
+    return you.apples, me.apples
+    
+def exchange_ideas(you, me):
+    # Share and combine the total number of ideas
+    total_ideas = you.ideas + me.ideas
+    you.ideas = total_ideas
+    me.ideas = total_ideas
+    return you.ideas, me.ideas
+
+exchange_apples(johanna, martin)
+print("Johanna has {} apples and Martin has {} apples".format(johanna.apples, martin.apples))
+exchange_ideas(johanna, martin)
+print("Johanna has {} ideas and Martin has {} ideas".format(johanna.ideas, martin.ideas))
